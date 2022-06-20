a274910fe53fac56ed4a677710530245440bc43e
Freddie Nelson <freddie0208@hotmail.com>
2022-06-20 19:52:00 +0100

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex justify-center items-center w-full h-full">
+      <h1 className="font-bold text-2xl text-blue-900">shorty.lol</h1>
+    </div>
+  );
+}
